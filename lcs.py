4d9d33640bfc8dfbc27bcefa333b336bb415dddb
Adityaa SS <adityaa.sureshbabu@gmail.com>
2025-04-14 22:51:24 +0530

    Updated LCS

--- lcs.py
+++ lcs.py
@@ -1,39 +1,34 @@
-def lcs(s1, s2, i, j):
-    if i == len(s1) or j == len(s2):
-        return 0
-    if s1[i] == s2[j]:
-        return 1 + lcs(s1, s2, i + 1, j + 1)
-    else:
-        return max(lcs(s1, s2, i + 1, j), lcs(s1, s2, i, j + 1))
+def longest_common_subsequence(str1, str2):
+    n = len(str1)
+    m = len(str2)
 
-def lcs_back(s1, s2, i, j, memo):
-    if i == len(s1) or j == len(s2):
-        return ""
+    dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
 
-    if (i, j) in memo:
-        length = memo[(i, j)]
-    else:
-        if s1[i] == s2[j]:
-            length = 1 + lcs(s1, s2, i + 1, j + 1)
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # Reconstruct the LCS string from the dp table
+    i, j = n, m
+    lcs_chars = []
+    while i > 0 and j > 0:
+        if str1[i - 1] == str2[j - 1]:
+            lcs_chars.append(str1[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
         else:
-            length = max(lcs(s1, s2, i + 1, j), lcs(s1, s2, i, j + 1))
-        memo[(i, j)] = length
+            j -= 1
 
-    if s1[i] == s2[j]:
-        return s1[i] + lcs_back(s1, s2, i + 1, j + 1, memo)
-    else:
-        len1 = lcs(s1, s2, i + 1, j)
-        len2 = lcs(s1, s2, i, j + 1)
-        if len1 > len2:
-            return lcs_back(s1, s2, i + 1, j, memo)
-        else:
-            return lcs_back(s1, s2, i, j + 1, memo)
+    lcs = "".join(reversed(lcs_chars))
+    return lcs
 
 if __name__ == "__main__":
-    str1 = "BDCABA"
-    str2 = "ABCBDAB"
-    length = lcs(str1, str2, 0, 0)
-    memo = {}
-    lcs1 = lcs_back(str1, str2, 0, 0, memo)
-    print(f"Length of LCS: {length}")
-    print(f"Longest Common Subsequence: {lcs1}")
+    str1 = input("Enter the first string: ")
+    str2 = input("Enter the second string: ")
+    result = longest_common_subsequence(str1, str2)
+    print("Longest Common Subsequence:", result)
